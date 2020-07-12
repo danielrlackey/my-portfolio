04081c2d930487fd1feac22f51e6c63d3b308a5e
danielrlackey <65509781+danielrlackey@users.noreply.github.com>
2020-07-12 14:53:50 -0400

    Add files via upload

--- my portfolio/portfolio.js
+++ my portfolio/portfolio.js
@@ -1,15 +1,15 @@
-let i = 0, text;
-text = "Daniel R Lackey"
+// let i = 0, text;
+// text = "Daniel R Lackey"
 
 
 
-function typing() {
-    if(i < text.length){
-        document.querySelector(".text").innerHTML += text.charAt(i);
-         i++;
-        setTimeout(typing, 150);
-    }
-}
+// function typing() {
+//     if(i < text.length){
+//         document.querySelector(".text").innerHTML += text.charAt(i);
+//          i++;
+//         setTimeout(typing, 150);
+//     }
+// }
 
 
-typing()
+// typing()
